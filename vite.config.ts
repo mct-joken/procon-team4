acded28eff3e71b7103c7c55df5d3e4c762148b2
kiharu3112 <kijiharu3112@outlook.jp>
2024-06-19 02:02:39 +0900

    fix:パスを修正した

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/procon-team1',
+  base: '/procon-team1/',
   build: {
     rollupOptions: {
       input: {
